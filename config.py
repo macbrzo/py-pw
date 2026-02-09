770abd15978c3df2266e77fe21775a67becbf929
MacBrzo <maciejbrzost@gmail.com>
2026-02-09 22:11:19 +0100

    Add dotenv

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 SCREENSHOTS_PATH = "artifacts/screenshots"
 VIDEOS_PATH = "artifacts/videos"
 
-BASE_URL = "https://www.twitch.tv"
+BASE_URL = getenv("BASE_URL", "https://www.twitch.tv")
+LONG_TIMEOUT = 10_000
+SHORT_TIMEOUT = 5_000
